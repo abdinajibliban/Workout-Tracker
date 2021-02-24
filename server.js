ab3c052c80c76b10a1c0ba976876bdc14f131459
Abdinajib Liban <abdinajibliban@gmail.com>
2021-02-23 22:21:36 -0500

    deploying prep

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_DB",
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false,
 });
 
